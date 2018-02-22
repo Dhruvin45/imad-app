ac8b99fc38820c67db5b6a0812de4cde20ec3d53
Dhruvin45 <36485327+Dhruvin45@users.noreply.github.com>
2018-02-22 22:27:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-1',function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/zrtical-2',function (req, res){
